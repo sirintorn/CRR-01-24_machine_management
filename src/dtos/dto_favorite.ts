ecf4c2a6767af23abfde3798673f8ddac8621c21
The Integrator <developer@theintegrator.co.th>
2024-11-19 17:43:32 +0700

    commit restore-backup bugs fixed

--- src/dtos/dto_favorite.ts
+++ src/dtos/dto_favorite.ts
@@ -17,6 +17,22 @@ export class DTOFavorite extends DTO{
         this.isCustom = data.is_custom;
     }
 
+    static convertFromArray(dtos: DTOFavorite[]){
+        let arr: Favorite[] = [];
+        for (let i = 0; i < dtos.length; i++) {
+            const item = dtos[i];
+            let fav: Favorite = {
+                id: item.id,
+                machine_id: item.machineId,
+                db_version_id: item.dbVersionId,
+                product_shade_id: item.productShadeId,
+                is_custom: item.isCustom
+            };
+            arr.push(fav);
+        }
+        return arr;
+    }
+
     static parseFromArray(dtos: Favorite[]){
         let arr: DTOFavorite[] = [];
         for (let i = 0; i < dtos.length; i++) {
--- src/routers/x_backup_restore.ts
+++ src/routers/x_backup_restore.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { FavoriteSchema } from "../models/m_favorites";
 import { Machine, MachineSchema } from "../models/m_machines";
+import { DTOFavorite } from "../dtos/dto_favorite";
 
 export const XBackupRestoreRoutes = Router();
 
@@ -12,7 +13,12 @@ XBackupRestoreRoutes.route(path + '/:target_machine').put(async (req, res) => {
         const backup = req.body;
 
         const db_version_id = backup.db_version_id;
-        let favorites: any[] = backup.json_data.favorites ?? [];
+        let favorites: DTOFavorite[] = backup.json_data.favorites ?? [];
+
+        for (let i = 0; i < favorites.length; i++) {
+            let item = favorites[i];
+            item.machineId = target_machine;
+        }
 
         /*
             UPDATE Machine default DBVersion
@@ -22,6 +28,7 @@ XBackupRestoreRoutes.route(path + '/:target_machine').put(async (req, res) => {
             RETURN favorites 
         */
 
+        
         const favSchema = new FavoriteSchema();
         const machineSchema = new MachineSchema();
 
@@ -29,21 +36,24 @@ XBackupRestoreRoutes.route(path + '/:target_machine').put(async (req, res) => {
         if(machine){
             machine.db_version_id = db_version_id;
             await machineSchema.update(target_machine, machine);
+
+            await favSchema.deleteByMachine(target_machine);
+
+            const rawFavs = DTOFavorite.convertFromArray(favorites);
+
+            const favIds = await favSchema.createMultiple(rawFavs, true);
+            for (let i = 0; i < favorites.length; i++) {
+                let item = favorites[i];
+                item.id = favIds[i].id;
+            }
+    
+            res.status(200).send({
+                machine: machine,
+                favorites: favorites
+            });
+        }else{
+            res.status(404).send();
         }
-
-        await favSchema.deleteByMachine(target_machine);
-
-        const favIds = await favSchema.createMultiple(favorites, true);
-        for (let i = 0; i < favorites.length; i++) {
-            let item = favorites[i];
-            item.id = favIds[i];
-            item.machine_id = target_machine;
-        }
-
-        res.status(200).send({
-            machine: machine,
-            favorites: favorites
-        });
     } catch (error) {
         res.status(400).send(error);
     }
